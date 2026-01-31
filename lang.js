5886c9e19396fe9e0ed61b9e5ccab7b21bef13a9
modubrix-boop <modubrix@gmail.com>
2026-01-31 12:10:55 +0800

    Update lang.js

--- lang.js
+++ lang.js
@@ -1,1 +1,16 @@
+function setLang(lang) {
+  document.querySelectorAll('[data-en]').forEach(el => {
+    el.textContent = el.getAttribute('data-' + lang);
+  });
+
+  document.querySelectorAll('.lang button').forEach(btn => {
+    btn.classList.remove('active');
+  });
+
+  document.getElementById('btn-' + lang).classList.add('active');
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  setLang('zh');
+});
 
